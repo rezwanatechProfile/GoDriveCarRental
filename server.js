3ade450feeab371dec3d839fd69dcc12fcb02aae
rezwanatechProfile <rezwanatech@gmail.com>
2023-03-17 21:01:03 -0400

    updating code for heroku

--- server.js
+++ server.js
@@ -65,7 +65,11 @@ app.get("/", (req,res)=> {
 
 
 // PORT
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on port ${PORT}`);
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
